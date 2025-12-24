Soilad/Spirits-Of-Steel-Rebirth

commit 606393a8b39aa485057ada40740b4d449a36f901

Removed SelectionManager.

Managers/MapManager.gd
@@ -315,11 +315,11 @@ func handle_click(global_pos: Vector2, map_sprite: Sprite2D) -> void:
 
 	var pid = get_province_with_radius(global_pos, map_sprite, 5)
 	if pid > 1:
-		if len(SelectionManager.selected_troops) > 0:
+		if !TroopManager.troop_selection.selected_troops.is_empty():
 			return
 		province_clicked.emit(pid, province_to_country.get(pid, ""))
-	else:
-		emit_signal("close_sidemenu")
+
+	close_sidemenu.emit()
 
 
 # To probe around and still register a click if we hit province/coutnry border

Managers/MainClock.gd
@@ -66,7 +66,6 @@ func set_speed(scale: float) -> void:
 	time_scale = clamp(scale, PAUSE, MAX_SPEED)
 
 
-
 func decreaseSpeed():
 	if is_paused():
 		set_speed(prev_time_scale)
@@ -77,20 +76,22 @@ func increaseSpeed():
 	if is_paused():
 		set_speed(prev_time_scale)
 	set_speed(time_scale + 15)
-	
 
 
 func is_paused() -> bool:
 	return time_scale <= PAUSE
+
 
 func pause() -> void:
 	if not is_paused():
 		prev_time_scale = time_scale
 		set_speed(PAUSE)
 
+
 func resume() -> void:
 	if is_paused():
 		set_speed(prev_time_scale)
+
 
 func toggle_pause() -> void:
 	if is_paused():

Managers/SelectionManager.gd
@@ -1,20 +1,0 @@
-# res://singletons/SelectionManager.gd
-## NOTE(pol): This is only used to store the selected troops in an array, 
-## because troop_renderer, TroopSelection and MapManager need it.
-extends Node
-
-const TROOP_DATA_TYPE = preload("res://Scripts/TroopData.gd")
-var selected_troops: Array[TROOP_DATA_TYPE] = []
-
-func select_troops(new_list: Array[TROOP_DATA_TYPE], append: bool = false) -> void:
-	if not append:
-		selected_troops.clear()
-	
-	for t in new_list:
-		if not selected_troops.has(t):
-			selected_troops.append(t)
-			
-	get_tree().call_group("TroopRenderer", "queue_redraw")
-
-func is_a_troop_selected() -> bool:
-	return len(selected_troops) > 0

Managers/SelectionManager.gd.uid
@@ -1,1 +1,0 @@
-uid://erx4xppo4e8t

Managers/TroopManager.gd
@@ -13,6 +13,8 @@ var path_cache: Dictionary = {}            # { start_id: { target_id: path_array
 var flag_cache: Dictionary = {}            # { country_name: texture }
 var needs_redraw := false                  # Used to throttle redraw calls
 
+var troop_selection: TroopSelection
+
 
 func _ready() -> void:
 	set_process(false)

Scenes/troop_renderer.gd
@@ -235,7 +235,7 @@ func _draw_single_troop_visual(troop: TroopData, pos: Vector2, player_country: S
 ## Helper to determine border color AND thickness based on owner
 func _get_troop_style(troop: TroopData, player_country: String) -> Dictionary:
 	var is_owner = troop.country_name.to_lower() == player_country
-	var is_selected = SelectionManager.selected_troops.has(troop)
+	var is_selected := TroopManager.troop_selection.selected_troops.has(troop)
 	
 	if is_owner:
 		if is_selected:

Scenes/world.gd
@@ -11,12 +11,12 @@ var map_width: float = 0.0
 
 
 func _ready() -> void:
-	#MapManager.map_ready.connect(_on_map_ready, CONNECT_ONE_SHOT)
-
 	await get_tree().process_frame # wait for Managers (singletons) to load
 	
 	if MapManager.id_map_image != null:
 		_on_map_ready()
+
+	TroopManager.troop_selection = $TroopSelection as TroopSelection
 
 
 func _on_map_ready() -> void:

Scenes/world.tscn
@@ -6,7 +6,7 @@
 [ext_resource type="Texture2D" uid="uid://ct2rdn4ldt5ks" path="res://maps/regions.png" id="3_5a7ea"]
 [ext_resource type="Script" uid="uid://cvkc8n0kgyk3c" path="res://Scenes/camera_controller.gd" id="4_8j60k"]
 [ext_resource type="Script" uid="uid://bko55up7cjg3g" path="res://Scenes/troop_renderer.gd" id="5_o06y3"]
-[ext_resource type="Script" uid="uid://dx8iwvng5oypc" path="res://Scripts/TroopSelection.gd" id="6_5ukr8"]
+[ext_resource type="Script" uid="uid://dx8iwvng5oypc" path="res://Scripts/troop_selection.gd" id="6_5ukr8"]
 
 [sub_resource type="ShaderMaterial" id="ShaderMaterial_8j60k"]
 

commit fedccffdcf9d9bfe77b85803a0b667dfdb89c65f

Fixed bug caused by 606393a.

Managers/MapManager.gd
@@ -314,12 +314,14 @@ func handle_click(global_pos: Vector2, map_sprite: Sprite2D) -> void:
 		return
 
 	var pid = get_province_with_radius(global_pos, map_sprite, 5)
-	if pid > 1:
-		if !TroopManager.troop_selection.selected_troops.is_empty():
-			return
-		province_clicked.emit(pid, province_to_country.get(pid, ""))
+	if pid <= 1:
+		close_sidemenu.emit()
+		return
 
-	close_sidemenu.emit()
+	if !TroopManager.troop_selection.selected_troops.is_empty():
+		return
+
+	province_clicked.emit(pid, province_to_country.get(pid, ""))
 
 
 # To probe around and still register a click if we hit province/coutnry border